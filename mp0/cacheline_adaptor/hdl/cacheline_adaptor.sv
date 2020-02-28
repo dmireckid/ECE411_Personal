b851cb2c202b25422148db770f4ab63178a7ada6
Damian Mirecki <mirecki2@illinois.edu>
2020-02-27 22:44:32 -0600

    fix clock cycle issue

--- mp0/cacheline_adaptor/hdl/cacheline_adaptor.sv
+++ mp0/cacheline_adaptor/hdl/cacheline_adaptor.sv
@@ -24,15 +24,22 @@ logic [63:0] burst0;
 logic [63:0] burst1;
 logic [63:0] burst2;
 logic [63:0] burst3;
+logic v1,v2,v3,v0;
 
 enum int unsigned{
   idle,
-  start,
-  s_burst0,
-  s_burst1,
-  s_burst2,
-  s_burst3,
-  finish
+  start_r,
+  start_w,
+  r_burst0,
+  r_burst1,
+  r_burst2,
+  r_burst3,
+  w_burst0,
+  w_burst1,
+  w_burst2,
+  w_burst3,
+  finish,
+  winish
 } state, next_state;
 
 always_comb
@@ -41,25 +48,69 @@ begin : state_actions
     read_o = 0;
     write_o = 0;
     resp_o = 0;
+    v1=0;
+    v2=0;
+    v3=0;
+    v0=0;
   end
   case(state)
     idle: begin
       read_o = 0;
       write_o = 0;
       resp_o = 0;
+      v1=0;
+      v2=0;
+      v3=0;
+      v0=0;
     end
-    start: begin
+    start_r: begin
       address_o = address_i;
       read_o = 1;
     end
-    s_burst0:
+    start_w: begin
+      address_o = address_i;
+      write_o = 1;
+    end
+    r_burst0: begin
       burst0 = burst_i;
-    s_burst1:
+      v0 = 1;
+      //$display("Burst 0 is: %h", burst0);
+      end
+    r_burst1: begin
       burst1 = burst_i;
-    s_burst2:
+      v1=1;
+      //$display("Burst 1 is: %h", burst1);
+      end
+    r_burst2: begin
       burst2 = burst_i;
-    s_burst3:
+      v2=1;
+      //$display("Burst 2 is: %h", burst2);
+      end
+    r_burst3: begin
       burst3 = burst_i;
+      v3=1;
+      //$display("Burst 3 is: %h", burst3);
+      end
+    w_burst0: begin
+      burst_o = line_i[63:0];
+      $display("Write 0 is: %h", burst_o);
+    end
+    w_burst1: begin
+      burst_o = line_i[127:64];
+      $display("Write 0 is: %h", burst_o);
+    end
+    w_burst2: begin
+      burst_o = line_i[191:128];
+      $display("Write 0 is: %h", burst_o);
+    end
+    w_burst3: begin
+      burst_o = line_i[255:192];
+      $display("Write 0 is: %h", burst_o);
+    end
+    winish: begin
+      write_o = 0;
+      resp_o = 1;
+    end
     finish: begin
       line_o = {burst3, burst2, burst1, burst0};
       read_o = 0;
@@ -73,24 +124,41 @@ begin : next_state_logic
   next_state = state;
   case(state)
     idle: begin
-      if(read_i == 1) next_state = start;
+      if(read_i == 1) next_state = start_r;
+      else if(write_i == 1) next_state = start_w;
     end
-    start: begin
-      if(read_o == 0 && resp_i == 1) next_state = s_burst0;
+    start_r: begin
+      if(resp_i == 1) next_state = r_burst0;
     end
-    s_burst0: begin
-      if(read_o == 0 && resp_i == 1) next_state = s_burst1;
+    start_w: begin
+      if(resp_i == 1) next_state = w_burst0;
     end
-    s_burst1: begin
-      if(read_o == 0 && resp_i == 1) next_state = s_burst2;
+    r_burst0: begin
+      next_state = r_burst1;
     end
-    s_burst2: begin
-      if(read_o == 0 && resp_i == 1) next_state = s_burst3;
+    r_burst1: begin
+      next_state = r_burst2;
     end
-    s_burst3: begin
-      if(resp_i == 0) next_state = finish;
+    r_burst2: begin
+      next_state = r_burst3;
+    end
+    r_burst3: begin
+      if(resp_i == 0 && v3) next_state = finish;
+    end
+    w_burst0: begin
+      next_state = w_burst1;
+    end
+    w_burst1: begin
+      next_state = w_burst2;
+    end
+    w_burst2: begin
+      next_state = w_burst3;
+    end
+    w_burst3: begin
+      if(resp_i==0) next_state = winish;
     end
     finish: next_state = idle;
+    winish: next_state = idle;
   endcase
 end
 
